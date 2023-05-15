1f390cd55138a00fc5b887cba297927d081b36b3
Viktor Kislyak <viktorkislyak1996@gmail.com>
2023-05-15 14:17:40 +0300

    add black codeformatter

--- core/core/utils/misc.py
+++ core/core/utils/misc.py
@@ -3,6 +3,6 @@ import yaml
 
 def yaml_coerce(value):
     if isinstance(value, str):
-        return yaml.load(f'dummy: {value}', Loader=yaml.SafeLoader)['dummy']
+        return yaml.load(f"dummy: {value}", Loader=yaml.SafeLoader)["dummy"]
 
     return value
---
+++ core/test2.py
@@ -0,0 +1,5 @@
+def test_func():
+    res1 = 1 + 2
+    res2 = 4 + 5
+
+    print(res1, res2)
